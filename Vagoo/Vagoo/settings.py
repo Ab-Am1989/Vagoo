b95216db7f8680a183944c1ca917b2bf2cd758bb
amini <abolfazl.amini1990@gmail.com>
2021-10-31 00:30:33 +0330

    accounts app has been added to project

--- Vagoo/Vagoo/settings.py
+++ Vagoo/Vagoo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'location_field.apps.DefaultConfig',
     'entities.apps.EntitiesConfig',
+    'perception.apps.PerceptionConfig',
 ]
 
 MIDDLEWARE = [
