5ffa6f4a340d4f53c5589bce63854e124034756e
amini <abolfazl.amini1990@gmail.com>
2021-10-31 11:01:29 +0330

    Find and solve a problem in admin.py of perception

--- Vagoo/perception/admin.py
+++ Vagoo/perception/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from perception.models import MoviePerception, BookPerception, TheaterPerception, SongPerception, JourneyPerception
-
 # Register your models here.
 
 admin.site.register(MoviePerception)
-admin.site.register(MoviePerception)
+admin.site.register(BookPerception)
 admin.site.register(TheaterPerception)
 admin.site.register(SongPerception)
 admin.site.register(JourneyPerception)
