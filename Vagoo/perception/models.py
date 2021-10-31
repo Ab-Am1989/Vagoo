5243596d5c101a005bd12f4fadb9d5f84f906064
amini <abolfazl.amini1990@gmail.com>
2021-10-31 10:19:26 +0330

    5 model for add user perception about our entities have been added to our models.py of perception app. We created an abstract class for CommonFields.

--- Vagoo/perception/models.py
+++ Vagoo/perception/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from django.db.models import CASCADE
 from entities.models import Movie, Book, Theater, Song, Journey
-from django.contrib.auth.models import User
+from accounts.models import Profile
 
 
 # Create your models here.
 
-class MoviePerception(models.Model):
-    # profile = models.ForeignKey('Profile')
-    entity = models.ForeignKey('entities.Movie', on_delete=CASCADE, verbose_name='فیلم')
+class CommonFields(models.Model):
+    class Meta:
+        abstract = True
+
+    profile = models.ForeignKey('accounts.Profile', on_delete=CASCADE, verbose_name='نمایه کاربر')
     perception = models.TextField('ادراک')
+
+
+class MoviePerception(CommonFields):
+    entity = models.ForeignKey('entities.Movie', on_delete=CASCADE, verbose_name='فیلم')
+
+
+class BookPerception(CommonFields):
+    entity = models.ForeignKey('entities.Book', on_delete=CASCADE, verbose_name='کتاب')
+
+
+class TheaterPerception(CommonFields):
+    entity = models.ForeignKey('entities.Theater', on_delete=CASCADE, verbose_name='تئاتر')
+
+
+class SongPerception(CommonFields):
+    entity = models.ForeignKey('entities.Song', on_delete=CASCADE, verbose_name='ترانه')
+
+
+class JourneyPerception(models.Model):
+    entity = models.ForeignKey('entities.Journey', on_delete=CASCADE, verbose_name='سفر')
