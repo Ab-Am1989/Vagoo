923a0c2aff83487497d1567804f97560118f4912
amini <abolfazl.amini1990@gmail.com>
2021-10-31 10:31:09 +0330

    Models of perception app have been registered on admin.py of this app

--- Vagoo/perception/admin.py
+++ Vagoo/perception/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from perception.models import MoviePerception, BookPerception, TheaterPerception, SongPerception, JourneyPerception
 
 # Register your models here.
+
+admin.site.register(MoviePerception)
+admin.site.register(MoviePerception)
+admin.site.register(TheaterPerception)
+admin.site.register(SongPerception)
+admin.site.register(JourneyPerception)
